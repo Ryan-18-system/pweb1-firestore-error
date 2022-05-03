8f380bfbe1f36cb1cb5b5e83754e50e868e0fe7a
Ryan Nóbrega Brandão <ryanbrandao18@gmail.com>
2022-05-03 13:06:23 -0300

    first commit

---
+++ firebase.config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyBnHFm6fsadK4RTvH1DV2vyhWeE9MVkgRA',
+  authDomain: 'social-pweb1.firebaseapp.com',
+  projectId: 'social-pweb1',
+  storageBucket: 'social-pweb1.appspot.com',
+  messagingSenderId: '418463618533',
+  appId: '1:418463618533:web:354c4c0249801e045a0b0c',
+  measurementId: 'G-210FX2X420'
+};
